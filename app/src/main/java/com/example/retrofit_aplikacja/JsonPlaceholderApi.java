f0aee4f3100f991af62f7733e11754792f47514f
Kamilo359 <kami.choroba7@o2.pl>
2025-12-02 12:10:45 +0100

    dalsza czesc

--- app/src/main/java/com/example/retrofit_aplikacja/JsonPlaceholderApi.java
+++ app/src/main/java/com/example/retrofit_aplikacja/JsonPlaceholderApi.java
@@ -1,4 +1,12 @@
 package com.example.retrofit_aplikacja;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface JsonPlaceholderApi {
+    @GET("pytania")
+
+    public Call<List<pytanie>>getPytania();
 }
